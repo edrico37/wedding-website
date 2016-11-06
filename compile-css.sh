57fd6bd7d039627ca02f92b02205b735923bdc1d
Dylan Hughes <dylan@zola.com>
2016-11-06 00:16:58 -0400

    initial commit to add Bootstrap LESS, index.html, and helper script

---
+++ compile-css.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+lessc ~/Sites/wedding/src/less/bootstrap/bootstrap.less ~/Sites/wedding/assets/bootstrap.css
+lessc ~/Sites/wedding/src/less/custom.less ~/Sites/wedding/assets/custom.css
